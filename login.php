739466009a269ae7a594320042da01651faa404b
Dylemmameow <72045399+Dylemmameow@users.noreply.github.com>
2021-12-05 06:58:39 +0200

    Lab login form

--- login.php
+++ login.php
@@ -1,52 +1,42 @@
-<?php 
-
-$host="localhost";
-$user="root";
-$password="";
-$db="Login";
-
-mysql_connect($host,$user,$password);
-mysql_select_db($db);
-
-if(isset($_POST['username'])){
-    
-    $uname=$_POST['username'];
-    $password=$_POST['password'];
-    
-    $sql="select * from loginform where user='".$uname."'AND Pass='".$password."' limit 1";
-    
-    $result=mysql_query($sql);
-    
-    if(mysql_num_rows($result)==1){
-        echo " V-ați autentificat cu succes";
-        exit();
-    }
-    else{
-        echo " Ați introdus o parolă incorectă";
-        exit();
-    }
-        
-}
-?>
-
-
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Logarea</title>
-	<link rel="stylesheet" a href="css\stylelogin.css">
+<meta charset="utf-8">
+<title>Login</title>
+<link rel="stylesheet" href="css/style.css" />
 </head>
 <body>
-	<div class="container">
-		<form>
-			<div class="form-input">
-				<input type="text" name="text" placeholder="Introduceți loginul"/>	
-			</div>
-			<div class="form-input">
-				<input type="password" name="password" placeholder="password"/>
-			</div>
-			<input type="submit" type="submit" value="LOGIN" class="btn-login"/>
-		</form>
-	</div>
+<?php
+require('db.php');
+session_start();
+if (isset($_POST['username'])){
+	$username = stripslashes($_REQUEST['username']);
+	$username = mysqli_real_escape_string($con,$username);
+	$password = stripslashes($_REQUEST['password']);
+	$password = mysqli_real_escape_string($con,$password);
+        $query = "SELECT * FROM `users` WHERE username='$username'
+and password='".md5($password)."'";
+	$result = mysqli_query($con,$query) or die(mysql_error());
+	$rows = mysqli_num_rows($result);
+        if($rows==1){
+	    $_SESSION['username'] = $username;
+	    header("Location: index.php");
+         }else{
+	echo "<div class='form'>
+<h3>Username/password is incorrect.</h3>
+<br/>Apasa aici <a href='login.php'>Login</a></div>";
+	}
+    }else{
+?>
+<div class="form">
+<h1>Log In</h1>
+<form action="" method="post" name="login">
+<input type="text" name="username" placeholder="Username" required />
+<input type="password" name="password" placeholder="Password" required />
+<br>
+<input name="submit" type="submit" value="Login" />
+</form>
+<p>Nu sunteti inregistrati? <a href='registration.php'>Registratie aici</a></p>
+</div>
+<?php } ?>
 </body>
 </html>
